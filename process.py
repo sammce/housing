1886887463c91c7ac058e6710dce32b4c98434dc
= <sammcelligott@outlook.com>
2021-02-02 16:59:36 +0000

    Added base ProcessedData object

--- process.py
+++ process.py
@@ -1,2 +1,14 @@
-def process():
-    pass
+from clean import CleanedData 
+
+class ProcessedData(CleanedData):
+
+    def __init__(self):
+        # create cleaned object
+        super().__init__()
+        print(self.years)
+
+    def percentage(self):
+        # percentage change from year to year
+        # percentage change decade to decade
+        # percentage change overall
+        pass
